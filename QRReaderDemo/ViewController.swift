56f96e1b068be01dfb8131b6255f227c0f0b978e
Saleem Siddiqui <saleem@saleem-siddiqui.com>
2016-04-19 14:21:02 -0500

    Updated year in copyright notice (testing the waters of iOS development)
    Removed extra whitespace
    
    Added first few steps from tutorial to ViewController. Code compiles but does not work :-(
    
    Added setMetadataObjectsDelegate() call in ViewController
    
    Added video preview layer to view
    
    Added start-session code
    
    Modified .gitignore to remove user-specific dir names

--- QRReaderDemo/ViewController.swift
+++ QRReaderDemo/ViewController.swift
@@ -9,21 +9,53 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     @IBOutlet weak var messageLabel:UILabel!
 
-    
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
 
+        do {
+            // Set the input device on the capture session.
+            let input = try AVCaptureDeviceInput.init(device: captureDevice)
+
+            // Initialize the captureSession object.
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input as AVCaptureInput)
+
+            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+
+            // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+
+            // Start video capture.
+            captureSession?.startRunning()
+
+            print("DONE")
+        }
+        catch _ {
+            print("Error")
+            return
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
